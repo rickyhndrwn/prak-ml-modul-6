624ac5db8d4ca48be7d55ad193994425f79499ae
Ricky Hendrawan <ricky.hndrwn98@gmail.com>
2021-12-30 20:40:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     db_model_dict, random_test_image_dir, random_test_image_name = init_db_values()
